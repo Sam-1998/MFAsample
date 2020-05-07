7d542ec80129cd5bd10d47527fb235e3d6b139bb
Sam-1998 <46895280+Sam-1998@users.noreply.github.com>
2020-05-07 11:37:20 +0200

    - Added Tables for car sharing - Attempted to add a wireless Execution to phone in other envirioments aswell.

---
+++ MFAsample_final/MFAsample_final/Tables/SharedCarKeyTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MFAsample_final.Tables
+{
+    class SharedCarKeyTable
+    {
+        public Guid UserId { get; set; }
+        public String Email { get; set; }
+        public String DateBooked { get; set; }
+        public String EndDate { get; set; }
+    }
+}
